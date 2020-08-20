1f4f1ce1043afb98aa4e2a9e92ca3ebb5cab7e46
Farshad Nematdoust <farshad@nematdoust.com>
2020-08-20 20:08:21 +0200

    Add DefaultApiTest.php this is just an empty file, I need to add the tests later.

---
+++ test/Api/DefaultApiTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace NCrypt\Client\Api;
+
+use PHPUnit\Framework\TestCase;
+
+class DefaultApiTest extends TestCase
+{
+
+    /**
+     * Setup before running any test cases
+     */
+    public static function setUpBeforeClass(): void
+    {
+    }
+
+    /**
+     * Setup before running each test case
+     */
+    public function setUp(): void
+    {
+    }
+
+    /**
+     * Clean up after running each test case
+     */
+    public function tearDown(): void
+    {
+    }
+
+    /**
+     * Clean up after running all test cases
+     */
+    public static function tearDownAfterClass(): void
+    {
+    }
+
+    /**
+     * Test case for notePost
+     *
+     * .
+     *
+     */
+    public function testNotePost(): void
+    {
+    }
+}
