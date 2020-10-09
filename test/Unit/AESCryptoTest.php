aa5c430ee23b294bd7d41ddaca79dfded6e402e1
firouze <firouzeemami@gmail.com>
2020-10-09 10:57:25 +0200

    add unit test for AESCrypto class

---
+++ test/Unit/AESCryptoTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NCrypt\Client;
+
+use NCrypt\Client\Library\Crypto\AESCrypto;
+use NCrypt\Client\Model\SecureNote;
+use PHPUnit\Framework\TestCase;
+
+class AESCryptoTest extends TestCase
+{
+
+    public function testReturnTypeValidatorForSecureNoteRequest()
+    {
+        $crypto = new AESCrypto();
+        $secureNoteResult = $crypto->secureNoteRequest("this is a test note");
+        $this->assertInstanceOf(SecureNote::class,$secureNoteResult);
+    }
+
+    public function testIvAndKeyLengthInSecureNoteRequest()
+    {
+        $crypto = new AESCrypto();
+        $secureNoteResult = $crypto->secureNoteRequest("this is a test note");
+
+        $lengthOfIv = strlen($secureNoteResult->getIv());
+        $this->assertEquals(16,$lengthOfIv);
+
+        $lengthOfKey = strlen($secureNoteResult->getKey());
+        $this->assertEquals(44,$lengthOfKey);
+
+    }
+
+
+}
